4c247a3b55553d6b16ec84b4eb9fe7a886583148
r0c0n <126270904+r0c0n@users.noreply.github.com>
2023-05-04 14:34:31 +0100

    Create raw_iter.rs

---
+++ benches/raw_iter.rs
@@ -0,0 +1,12 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use dbce::baserules::board::PSBoard;
+
+fn raw_iter(c: &mut Criterion) {
+    let board = PSBoard::default().board;
+    c.bench_function("RawBoard-Iterate", |b| {
+        b.iter(|| black_box(board).into_iter().filter(|p| p.is_none()).count())
+    });
+}
+
+criterion_group!(benches, raw_iter);
+criterion_main!(benches);
